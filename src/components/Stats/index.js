c43c1cf7fa8d4fa9eb40ef07a44f34cbae57e054
José Algorta <50190637+jopealgorta@users.noreply.github.com>
2023-06-17 00:21:38 +0000

    Improve stats.

--- src/components/Stats/index.js
+++ src/components/Stats/index.js
@@ -3,12 +3,18 @@ import './style.css';
 const Stats = ({ items }) => {
   const itemsCount = items.length;
   const packedItemsCount = items.filter((item) => item.packed).length;
+  const packedItemsPercentage = Math.round(
+    (packedItemsCount / itemsCount) * 100
+  );
+
   return (
     <footer className='stats'>
       <em>
-        🧳 You have {itemsCount} items on your list, and you have already packed{' '}
-        {packedItemsCount} (
-        {((packedItemsCount / (itemsCount || 1)) * 100).toFixed(0)} %)
+        {itemsCount === 0
+          ? '📦 Start packing!'
+          : packedItemsPercentage === 100
+          ? 'You got everything! 🎉 Ready to go ✈️'
+          : `🧳 You have ${itemsCount} items on your list, and you have already packed ${packedItemsCount} (${packedItemsPercentage} %)`}
       </em>
     </footer>
   );
